b528f074132b49dfd88c12b537aeb570a106de50
adiyanhehe <adiyanhehe@users.noreply.github.com>
2026-06-17 22:55:44 +0700

    fixed

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,3 @@
 import { createAuthClient } from '@neondatabase/auth';
 
-export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_NEON_AUTH_URL,
-});
+export const authClient = createAuthClient(import.meta.env.VITE_NEON_AUTH_URL);
